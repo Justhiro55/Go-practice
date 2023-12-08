6e084c82b9eaf95bbaaf40c6e34429659d647631
JustHiro55 <hagi.hiro9829@gmail.com>
2023-12-08 15:46:48 +0900

    go-00

---
+++ go-00/ex03/main.go
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.go                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhagiwar <hhagiwar@student.42Tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 13:11:38 by hhagiwar          #+#    #+#             */
+/*   Updated: 2023/10/02 19:10:05 by hhagiwar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+package main
+import "ex03/piscine"
+func main() {
+piscine.IsNegative(1)
+piscine.IsNegative(0)
+piscine.IsNegative(-1)
+}
